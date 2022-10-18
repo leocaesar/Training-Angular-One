d50098cf46f4bea767a674bbffc8837a4431b3ab
leocaesar <leocaesar98@gmail.com>
2022-10-18 11:56:37 +0700

    Add files via upload

---
+++ Task 1 - Format Date.js
@@ -0,0 +1,23 @@
+let date1= new Date()
+
+//1
+console.log(date1.toString())
+
+//2
+console.log(date1.toDateString())
+
+//3
+console.log(date1.toLocaleString())
+
+//4
+console.log(date1.toLocaleDateString())
+
+//5
+console.log(date1.toGMTString())
+
+//6
+console.log(date1.toUTCString())
+
+//7
+console.log(date1.toISOString())
+
